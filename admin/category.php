6fc7e7e78fca62ee83831e166527d11c910d9782
NT-Huyen <2051060557@e.tlu.edu.vn>
2023-02-22 22:53:19 +0700

    category

--- admin/category.php
+++ admin/category.php
@@ -7,9 +7,10 @@
     <title>Music for Life</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="css/style_login.css">
+    <link rel="stylesheet" href="css/style_category_baiviet.css">
 </head>
 <body>
+    
     <header>
         <nav class="navbar navbar-expand-lg bg-body-tertiary shadow p-3 bg-white rounded">
             <div class="container-fluid">
@@ -28,13 +29,13 @@
                         <a class="nav-link" href="../index.php">Trang ngoài</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link active fw-bold" href="category.php">Thể loại</a>
+                        <a class="nav-link  " href="category.php">Thể loại</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="author.php">Tác giả</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="article.php">Bài viết</a>
+                        <a class="nav-link active fw-bold" href="article.php">Bài viết</a>
                     </li>
                 </ul>
                 </div>
@@ -42,50 +43,72 @@
         </nav>
 
     </header>
-    <main class="container mt-5 mb-5">
-        <!-- <h3 class="text-center text-uppercase mb-3 text-primary">CẢM NHẬN VỀ BÀI HÁT</h3> -->
-        <div class="row">
-            <div class="col-sm">
-                <a href="add_category.php" class="btn btn-success">Thêm mới</a>
+    <?php
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $conn = new mysqli($servername, $username, $password,'btth01_cse485');
+
+        // Check connection
+        if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+        }
+        $sql = "select * from baiviet";
+        
+        $result = $conn->query($sql);
+        
+    ?>
+    <div class="container-fluid">
+    <a href="add_category.php" class="btn btn-success"style="margin: 50px 0 50px 0;">Thêm mới</a>
+        <div class="card">
+            <div class="card-body">
                 <table class="table">
-                    <thead>
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Tên thể loại</th>
+                    <thead class="thead-dark">
+                        <tr >
+                            <th>STT</th>
+                            <th>Tiêu đề</th>
+                            <th>Tên bài hát</th>
+                            <th>Mã thể loại</th>
+                            <th>Tóm tắt</th>
+                            <th>Nội dung</th>
+                            <th>Mã tác giả</th>
+                            <th>Ngày viết</th>
+                            <th>Hình ảnh</th>
                             <th>Sửa</th>
                             <th>Xóa</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>Nhạc trữ tình</td>
-                            <td>
-                                <a href="edit_category.php?id=1"><i class="fa-solid fa-pen-to-square"></i></a>
-                            </td>
-                            <td>
-                                <a href=""><i class="fa-solid fa-trash"></i></a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <th scope="row">2</th>
-                            <td>Nhạc cách mạng</td>
-                            <td>
-                                <a href="edit_category.php?id=2"><i class="fa-solid fa-pen-to-square"></i></a>
-                            </td>
-                            <td>
-                                <a href=""><i class="fa-solid fa-trash"></i></a>
-                            </td>
-                        </tr>
+                        <?php
+                            while($row = $result->fetch_assoc()){?>
+                                <tr >
+                                    <td><?php echo $row['ma_bviet']?></td>
+                                    <td><?php echo $row['tieude']?></td>
+                                    <td><?php echo $row['ten_bhat']?></td>
+                                    <td><?php echo $row['ma_tloai']?></td>
+                                    <td><?php echo $row['tomtat']?></td>
+                                    <td><?php echo $row['noidung']?></td>
+                                    <td><?php echo $row['ma_tgia']?></td>
+                                    <td><?php echo $row['ngayviet']?></td>
+                                    <td>
+                                        <img src="img/<?php echo $row['hinhanh'];?>">
+                                    </td>
+                                    
+                                    <td><a href="edit_category.php?id=2"><i class="fa-solid fa-pen-to-square"></i></a></td>
+                                    <td><a href=""><i class="fa-solid fa-trash"></i></a></td>
+                                </tr>
+                           <?php } ?>
+                        
                        
                     </tbody>
                 </table>
             </div>
-        </div>
-    </main>
+            
+        <div>
+        
+    </div>
     <footer class="bg-white d-flex justify-content-center align-items-center border-top border-secondary  border-2" style="height:80px">
         <h4 class="text-center text-uppercase fw-bold">TLU's music garden</h4>
     </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
-</html>
